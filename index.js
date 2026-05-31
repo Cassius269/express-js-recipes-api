b8678fada5bc048234f6e5e8e66fddbc9354cb40
cassius269 <fahamygaston@gmail.com>
2026-05-31 21:03:18 +0200

    ajout de la configuration CORS pour la production

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const cookie = require('cookie-parser');
 require('./database'); // importe et exécute la connexion MongoDB
 
 const app = express(); // création d'une instance d'Express
+
+// Actification du cors avant les routes
+app.use(cors({
+  origin: process.env.URL_API,
+  credentials: true
+}));
+
 const routes = require('./routes'); // récupérer les routes
 
 app.use(express.json()); // transformer le body en json
